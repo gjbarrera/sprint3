b2d8d28410f028b6a55a8227470915b373a9a1f2
gjbarrera <gjbarrera@uninorte.edu.co>
2021-10-13 22:31:26 -0500

    Mockups infoestudiante, buscador de cursos y login

--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from wtforms import Form, StringField, PasswordField, BooleanField, SubmitField, validators, TextAreaField
 
+
+    
 class Formulario_Contacto(Form):
     nombre = StringField('Nombre y Apellido', 
     [ 
@@ -16,5 +18,17 @@ class Formulario_Contacto(Form):
         validators.DataRequired('Campo Requerido para contacto')
     ])
     enviar = SubmitField('Enviar')
-    
 
+class Formulario_Ingresar(Form):
+    usuario = StringField('Usuario', 
+    [ 
+        validators.DataRequired('Campo Requerido para ingresar'), 
+        validators.Length(min=8,max=25)
+    ] )
+    contraseña = StringField('Contraseña',
+    [ 
+        validators.DataRequired('Campo Requerido para ingresar'), 
+        validators.Length(min=8,max=25) 
+    ])
+    
+    login = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, url_for
-from werkzeug.utils import html #agregada por franklin
 from utils import isUsernameValid, isEmailValid, isPasswordValid, isNameValid
 import yagmail as yagmail
-from forms import Formulario_Contacto
+from forms import Formulario_Contacto, Formulario_Ingresar 
 
 
 app = Flask(__name__)
@@ -12,6 +11,9 @@ app.secret_key = '_5#y2L"F4Q8z\n\xec]/'
 @app.route('/index')
 def index():    
     return render_template("index.html", titulo='Escuela Colombiana de Ingeniería Julio Garavito')
+
+
+
 
 #Formulario de Contacto
 @app.route('/contacto', methods=['GET', 'POST'])
@@ -48,11 +50,13 @@ def contacto():
     except:
         flash("¡Ups! Ha ocurrido un error, intentelo de nuevo.")
         return render_template("contacto.html", titulo="Formulario de contacto")
+    
+    #form = Formulario_Contacto(  request.form  )
+    #if request.method == 'POST' and form.validate():            
+        #flash('Inicio de sesión solicitado por el usuario {}, recordar={}'.format(form.usuario.data, form.recordar.data))
+        #return redirect(url_for('gracias'))
 
-
-@app.route('/ingresar')
-def ingresar():    
-    return render_template("ingresar.html")
+    #return render_template("contacto.html", form=form, titulo="Formulario de contacto")
 
 #Decoradores Panel Administrativo
 @app.route('/comentariosactividad', methods=['GET', 'POST'])
@@ -109,22 +113,59 @@ def registro():
 def gracias():
     return render_template("gracias.html", titulo='Gracias')
 
-#franklin
-@app.route('/infodocente', methods=['GET', 'POST'])
-def informacionDocente():
-    return render_template("admin/infodocente.html", titulo="Información de Docente")
 
-#Notas Estudiante
-@app.route('/notaestudiante', methods=['GET', 'POST'])
-def notasEstudiante():
-    return render_template("admin/notasestudiante.html", titulo="Calificaciones de Estudiante")
 
-#Notas docente
-@app.route('/notadocente', methods=['GET', 'POST'])
-def notadocente():
-    return render_template("admin/notasdocente.html", titulo="Calificaciones de Estudiante")
+
+#Formulario de Ingreso
+@app.route('/ingresar', methods=['GET', 'POST'])
+def ingresar():
+    try:
+        form = Formulario_Ingresar(  request.form  )
+        error = None
+        if request.method == 'POST': #and form.validate():  
+            usuario = request.form['Usuario']
+            contraseña = request.form['contraseña']
+            
+
+            #1. Validar datos de ingreso:
+            if not isNameValid(usuario):
+                # Si está mal.
+                error = "Solo debe usar letras en Usuario"
+                flash(error)
+            if not isEmailValid(contraseña):
+                # Si está mal.
+                error = "contraseña invalida"
+                flash(error)
+            if error is not None:
+                # Ocurrió un error
+                return render_template("ingresar.html", form=form, titulo="Iniciar Sesión")
+            else:
+                #2. Enviar un correo.
+                # Para crear correo:                                    
+                # Modificar la siguiente linea con tu informacion personal            
+                yag = yagmail.SMTP('yeffersone@uninorte.edu.co','39VMbtj_2EZ6jZ-')
+                yag.send(to='yeffersone@uninorte.edu.co', subject='ingresar web, '+usuario, headers={"Reply-To":f"{contraseña}"})
+                return render_template("gracias.html", titulo='Gracias por escribirnos')
+
+        return render_template("ingresar.html", form=form, titulo="Iniciar Sesión")
+    except:
+        flash("¡Ups! Ha ocurrido un error, intentelo de nuevo.")
+        return render_template("ingresar.html", titulo="Iniciar Sesión")
+
+
 
 #Decorador buscador de cursos
 @app.route('/busquedacursos', methods=['GET', 'POST'])
 def busqueda_cursos():
     return render_template("admin/busquedacursos.html", titulo="Buscador de cursos")
+
+#Decoradores informacion estudiante
+@app.route('/infoestudiante', methods=['GET', 'POST'])
+def informacion_Estudiante():
+    return render_template("admin/infoestudiante.html", titulo="Información del Estudiante")
+
+
+#Decoradores informacion docente
+@app.route('/infodocente', methods=['GET', 'POST'])
+def informacionDocente():
+    return render_template("admin/infodocente.html", titulo="Información de Docente")
